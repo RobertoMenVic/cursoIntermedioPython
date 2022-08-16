39ebc7f9814e9a8b4df53b02d196e3a298fbab66
Roberto Mendoza <robertoj199526@gmail.com>
2022-08-16 13:15:12 -0500

    Se crea un nuevo archivo polindromoException

--- debugging.py
+++ debugging.py
@@ -1,6 +1,31 @@
-def run():
-    divisor= lambda num: [i for i in range(1,num+1) if num%i==0]
-    print("Los numeros son:"+str(divisor(int(input("Ingresa un numero:")))))
+from csv import excel
+
+
+def run(num):
+    """divisor= lambda num: [i for i in range(1,num+1) if num%i==0]
+    print("Los numeros son:"+str(divisor(int(input("Ingresa un numero:")))))"""
+    divisor=[i for i in range(1,num+1) if num%i==0]
+    return divisor
+
+def validarDatoIngresado():
+    flag=True
+    while flag:
+        try:            
+            num=int(input("Ingresa un numero:"))
+            try: 
+                if num<=0:
+                    raise ValueError("No puedes agregar un numero negativo")
+                print(run(num))
+                flag=False
+            except ValueError as ve:
+                print(ve)
+                flag=True
+
+        except ValueError:
+            print("Debes de ingresar un numero")
+            flag=True
 
 if __name__=="__main__":
-    run()
+    validarDatoIngresado()
+        
+        
